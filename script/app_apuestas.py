5982d1e57723d63593b5a2fe44e47490e1c3f7a6
Joel Alejandro Molina Lopez <alejandrojoel790@gmail.com>
2026-04-21 09:47:00 -0500

    app

--- script/app_apuestas.py
+++ script/app_apuestas.py
@@ -5,7 +5,7 @@ from scipy.stats import poisson
 import os
 
 # --- 1. CONFIGURACION DE LA INTERFAZ ---
-st.set_page_config(page_title="Prototipo de Apuestas", layout="wide")
+st.set_page_config(page_title="Prototipo de Apuestas 2026", layout="wide")
 
 st.markdown("""
     <style>
